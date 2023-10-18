2f46942cc180a3937491e3406f086b190d50f377
Dhruv786Sharma <115816524+Dhruv786Sharma@users.noreply.github.com>
2023-10-18 22:58:44 +0530

    Update chat_room.js

--- chat_room.js
+++ chat_room.js
@@ -52,5 +52,5 @@ function logout()
 {
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
-      window.location = "chat.html";
+      window.location = "index.html";
 }
